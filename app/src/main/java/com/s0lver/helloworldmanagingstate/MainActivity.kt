69f1fe31099869d5571ee1825037e40c6dcbc9ba
Rafael Perez <rperez.github@gmail.com>
2026-06-16 21:45:55 -0700

    Request weather forecast rather than current weather
    - Request weather forecast.
    - Remove current weather field from the class.
    - Rename CurrentWeather data class to WeatherRecord
    - TODO: some todos were added
    
    TEST=Manually tested.

--- app/src/main/java/com/s0lver/helloworldmanagingstate/MainActivity.kt
+++ app/src/main/java/com/s0lver/helloworldmanagingstate/MainActivity.kt
@@ -26,9 +26,17 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.s0lver.helloworldmanagingstate.ui.theme.HelloWorldManagingStateTheme
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Date
 
 private const val API_URL =
     "https://api.open-meteo.com/v1/forecast?latitude=37.3688&longitude=-122.0363&daily=temperature_2m_max,temperature_2m_min&timezone=America%2FLos_Angeles&forecast_days=1&&current=weather_code,temperature_2m"
+
+private const val FORECAST_API_URL =
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=America%2FLos_Angeles&start_date=%DATE%&end_date=%DATE%"
+
+// TODO: Fix this warning, there might be a better class for this nowadays
+private val sdf = SimpleDateFormat("yyyy-MM-dd")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,13 +44,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             var currentWeather by remember { mutableStateOf<WeatherRecord?>(null) }
+            var currentDate by remember { mutableStateOf(Date()) }
 
             HelloWorldManagingStateTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         weatherRecord = currentWeather,
+                        date = currentDate,
                         modifier = Modifier.padding(innerPadding),
-                        onWeatherFetched = { currentWeather = it })
+                        onWeatherFetched = { record, date ->
+                            currentWeather = record
+                            currentDate = date
+                        })
                 }
             }
         }
@@ -52,16 +65,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(
     weatherRecord: WeatherRecord?,
+    date: Date,
     modifier: Modifier = Modifier,
-    onWeatherFetched: (WeatherRecord) -> Unit
+    onWeatherFetched: (WeatherRecord, Date) -> Unit
 ) {
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
 
     Column {
+        // TODO: Confirm that date has a value, here this is being used without that check and using
+        //  actually the check for the weather record instance.
         weatherRecord?.let {
             Text(
-                text = "Weather is ${it.weather} currently ${it.temperature}, min: ${it.minTemperature}, max: ${it.maxTemperature}",
+                text = "Weather for ${sdf.format(date)} will be ${it.weather} min: ${it.minTemperature}, max: ${it.maxTemperature}",
                 modifier = modifier
             )
         } ?: Text(text = "Weather not fetched yet", modifier = modifier)
@@ -71,32 +87,29 @@ fun Greeting(
             scope.launch {
                 // No need to wrap this in withContext(Dispatchers.IO) anymore!
                 // The function handles its own threading.
-                fetchWeatherData(context, onWeatherFetched)
+                // TODO: Need to handle that day gets its successor after each click
+                fetchWeatherData(context, date, onWeatherFetched)
             }
         }) { Text(text = "Fetch weather data?") }
     }
 }
 
-fun fetchWeatherData(context: Context, onResult: (WeatherRecord) -> Unit) {
+fun fetchWeatherData(context: Context, date: Date, onResult: (WeatherRecord, Date) -> Unit) {
     val queue = Volley.newRequestQueue(context)
-
-    val jsonRequest = JsonObjectRequest(Request.Method.GET, API_URL, null, { response ->
+    val preparedUrl = FORECAST_API_URL.replace("%DATE%", sdf.format(date))
+    val jsonRequest = JsonObjectRequest(Request.Method.GET, preparedUrl, null, { response ->
         // Success: response is a JSONObject
         val dailyNode = response.getJSONObject("daily")
         val minTemperature = dailyNode.getJSONArray("temperature_2m_min").getDouble(0)
         val maxTemperature = dailyNode.getJSONArray("temperature_2m_max").getDouble(0)
-
-        val currentNode = response.getJSONObject("current")
-        val weatherCode = currentNode.getInt("weather_code")
-        val currentTemperature = currentNode.getDouble("temperature_2m")
-
+        val weatherCode = dailyNode.getJSONArray("weather_code").getInt(0)
         val weather = Weather.fromCode(weatherCode)
-        val newWeather =
-            WeatherRecord(weather, currentTemperature, minTemperature, maxTemperature)
+
+        val newWeather = WeatherRecord(weather, minTemperature, maxTemperature)
 
         // Passing a different reference back in the event causes the Composable to see that a
         // new object is returned and forcing the UI update
-        onResult(newWeather)
+        onResult(newWeather, date)
     }, { error ->
         // Error: handle the exception
         Log.e("Network", "That didn't work: ${error.message}")
@@ -109,6 +122,6 @@ fun fetchWeatherData(context: Context, onResult: (WeatherRecord) -> Unit) {
 @Composable
 fun GreetingPreview() {
     HelloWorldManagingStateTheme {
-        Greeting(null, onWeatherFetched = {})
+        Greeting(weatherRecord = null, date = Date(), onWeatherFetched = { _, _ -> })
     }
 }
--- app/src/main/java/com/s0lver/helloworldmanagingstate/WeatherRecord.kt
+++ app/src/main/java/com/s0lver/helloworldmanagingstate/WeatherRecord.kt
@@ -2,7 +2,6 @@ package com.s0lver.helloworldmanagingstate
 
 data class WeatherRecord(
     val weather: Weather,
-    val temperature: Double,
     val minTemperature: Double,
     val maxTemperature: Double
 )
